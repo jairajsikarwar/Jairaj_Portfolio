3c973d3286bb636a9126eb8399678be5251d071f
Jairaj Sikarwar <jairaj.sikarwar6568@gmail.com>
2024-06-07 14:05:01 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,10 @@ const Hero = () => {
       <div className='flex flex-wrap'>
         <div className='w-full lg:w-1/2'>
           <div className='flex flex-col items-center lg:items-sta'> 
-            <motion.h1  animate={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 270, 270, 0],
-              borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-            }} className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Jairaj Sikarwar</motion.h1>
+            <motion.h1 
+             className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Jairaj Sikarwar</motion.h1>
            <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Frontend Developer</span>
-           <p className='my-2 max-w-6xl ml-20 py-6 font-light tracking-tight'>{HERO_CONTENT}</p> 
+           <p className='my-2 max-w-6xl ml-20 py-6 font-light tracking-tight'>{HERO_CONTENT}</p>
            </div>
         </div>
         <div className='w-full lg:w-1/2 lg:p-8'>
